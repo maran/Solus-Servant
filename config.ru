fcef6e7cb5abce30b1b277089e22c961ab29c67f
Maran Hidskes <maran.hidskes@gmail.com>
2011-12-15 14:36:28 +0100

    Added passenger support

---
+++ config.ru
@@ -0,0 +1,5 @@
+require 'rubygems'
+require 'sinatra'
+require './solus_servant.rb'
+
+run Sinatra::Application
--- solus_servant.rb
+++ solus_servant.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
 
-# Config operations
+# OpenVZ change operations
 get '/vz/change/:disk_number' do
   system("sed -i 's|VE_ROOT=/disk[1-4]/|VE_ROOT=/disk#{params[:disk_number]}/|' /etc/vz/vz.conf")
   system("sed -i 's|VE_PRIVATE=/disk[1-4]/|VE_PRIVATE=/disk#{params[:disk_number]}/|' /etc/vz/vz.conf")
 end
+
