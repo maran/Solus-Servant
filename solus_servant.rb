c7632b40f80bbdf1f2ce85e276963be229196221
Maran Hidskes <maran.hidskes@gmail.com>
2011-12-15 19:25:44 +0100

    Liil changes

--- solus_servant.rb
+++ solus_servant.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 # OpenVZ change operations
 get '/vz/change/:disk_number' do
-  system("sed -i 's|VE_ROOT=/disk[1-4]/|VE_ROOT=/disk#{params[:disk_number]}/|' /etc/vz/vz.conf")
-  system("sed -i 's|VE_PRIVATE=/disk[1-4]/|VE_PRIVATE=/disk#{params[:disk_number]}/|' /etc/vz/vz.conf")
+  system("sed -ibak 's|VE_ROOT=/disk[1-4]/|VE_ROOT=/disk#{params[:disk_number]}/|' /etc/vz/vz.conf")
+  system("sed -ibak 's|VE_PRIVATE=/disk[1-4]/|VE_PRIVATE=/disk#{params[:disk_number]}/|' /etc/vz/vz.conf")
 end
 
